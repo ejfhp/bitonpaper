35b23f4749caeed01a193217078c43deb558cdd1
Diego <ejfhp5@gmail.com>
2020-12-25 23:57:20 +0100

    WIP -- Merry Christmas --

--- flutter/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ flutter/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -1,6 +1,7 @@
 package io.flutter.plugins;
 
 import io.flutter.plugin.common.PluginRegistry;
+import net.nfet.flutter.printing.PrintingPlugin;
 
 /**
  * Generated file. Do not edit.
@@ -10,6 +11,7 @@ public final class GeneratedPluginRegistrant {
     if (alreadyRegisteredWith(registry)) {
       return;
     }
+    PrintingPlugin.registerWith(registry.registrarFor("net.nfet.flutter.printing.PrintingPlugin"));
   }
 
   private static boolean alreadyRegisteredWith(PluginRegistry registry) {
--- flutter/ios/Runner/GeneratedPluginRegistrant.m
+++ flutter/ios/Runner/GeneratedPluginRegistrant.m
@@ -4,9 +4,16 @@
 
 #import "GeneratedPluginRegistrant.h"
 
+#if __has_include(<printing/PrintingPlugin.h>)
+#import <printing/PrintingPlugin.h>
+#else
+@import printing;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [PrintingPlugin registerWithRegistrar:[registry registrarForPlugin:@"PrintingPlugin"]];
 }
 
 @end
