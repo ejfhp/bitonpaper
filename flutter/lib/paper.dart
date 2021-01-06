e39b947ae85239ecc0fe5a227cf159439e211b10
Diego <ejfhp5@gmail.com>
2021-01-06 12:41:31 +0100

    WIP - not compile

--- flutter/lib/paper.dart
+++ flutter/lib/paper.dart
@@ -1,19 +1,56 @@
 import 'dart:ui' as ui;
-import 'package:dartsv/dartsv.dart';
+import 'package:flutter/services.dart';
 
-class Wallet {
-  String privateKey;
-  String publicAddress;
-  ui.Image art;
-  ui.Image overlay;
+import 'wallet.dart';
 
-  Wallet() {
-    SVPrivateKey privKey = SVPrivateKey(networkType: NetworkType.MAIN);
-    this.privateKey = privKey.toWIF();
-    this.publicAddress = privKey.toAddress().toString();
+class Paper {
+  final Wallet wallet;
+  ui.Image _background;
+  ui.Image _overlay;
+  ByteData _backgroundData;
+  ByteData _overlayData;
+  int _height;
+  int _width;
+
+  Paper({this.wallet, ui.Image bgdImage, ui.Image overlayImage, ByteData bgdData, ByteData overlayData}) {
+    assert(this.wallet != null);
+    assert(bgdImage != null);
+    assert(overlayImage != null);
+    int bgH = bgdImage.height;
+    int ovH = overlayImage.height;
+    assert(bgH == ovH);
+    int bgW = bgdImage.width;
+    int ovW = overlayImage.width;
+    assert(bgW == ovW);
+    this._height = bgH;
+    this._width = bgW;
+    this._background = bgdImage.clone();
+    this._overlay = overlayImage.clone();
+    this._backgroundData = bgdData;
+    this._overlayData = overlayData;
   }
 
-  bool isReady() {
-    return pkQr != null && adQr != null && pkImg != null && adImg != null;
+  int get width {
+    return this._width;
+  }
+
+  int get height {
+    return this._height;
+  }
+
+  ui.Image get overlay {
+    return this._overlay.clone();
+  }
+
+  ui.Image get background {
+    return this._background.clone();
+  }
+
+  ByteData get overlayData {
+    return this._overlayData;
+  }
+
+  ByteData get backgroundData {
+    return this._backgroundData;
   }
 }
--- flutter/lib/walletPainter.dart
+++ flutter/lib/walletPainter.dart
@@ -10,72 +10,6 @@ import 'package:flutter/services.dart';
 import 'art.dart';
 import 'wallet.dart';
 
-class WalletPainter extends CustomPainter {
-  final ui.Image image;
-  final Art art;
-
-  WalletPainter({this.image, this.art}) {
-    print("Art image width, height: " + this.image.width.toString() + " " + this.image.height.toString());
-    print("Art width, height: " + this.art.width.toString() + " " + this.art.height.toString());
-  }
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    print("Paint 1");
-    ui.Paint p = ui.Paint();
-    print("Paint DRAW IMAGE");
-    canvas.save();
-    print("Disposed? " + image.debugDisposed.toString());
-    print("Runtime type: " + image.runtimeType.toString());
-    // canvas.drawImage(image, Offset.zero, p);
-    canvas.drawImageRect(image, ui.Rect.fromLTRB(0, 0, 10, 10), ui.Rect.fromLTRB(0, 0, 10, 10), p);
-    // print("Paint DRAW IMAGE DONE");
-
-    // p.color = Colors.green;
-    // canvas.drawRect(ui.Rect.fromLTRB(0, 0, 10, 10), p);
-    // p.color = Colors.red;
-    // canvas.drawRect(ui.Rect.fromLTRB(10, 10, 20, 20), p);
-    // p.color = Colors.blue;
-    // canvas.drawRect(ui.Rect.fromLTRB(30, 30, 40, 40), p);
-    // print("Paint DRAW RECT DONE");
-    // TextSpan span = new TextSpan(style: new TextStyle(color: Colors.black, fontSize: 30, fontFamily: "Roboto"), text: "BOP!");
-    // TextPainter tp = new TextPainter(text: span, textAlign: TextAlign.left, textDirection: TextDirection.ltr);
-    // tp.layout();
-    // tp.paint(canvas, new Offset(0, 0));
-    image.dispose();
-    print("Paint DRAW TEXT DONE");
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldPainter) {
-    return true;
-  }
-}
-
-class TextImage extends CustomPainter {
-  String text;
-  double fontSize;
-  String fontFamily;
-  ui.Color fontColor;
-  ui.Color bgColor;
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    ui.Paint p = ui.Paint();
-    p.color = this.bgColor;
-    canvas.drawRect(ui.Rect.fromLTRB(0, 0, size.width, size.height), p);
-    TextSpan span = new TextSpan(style: new TextStyle(color: this.fontColor, fontSize: this.fontSize, fontFamily: this.fontFamily), text: this.text);
-    TextPainter tp = new TextPainter(text: span, textAlign: TextAlign.left, textDirection: TextDirection.ltr);
-    tp.layout();
-    tp.paint(canvas, new Offset(0, 0));
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldPainter) {
-    return true;
-  }
-}
-
 class Rasterizer {
   Future<Uint8List> toQrCodeImg({String text, double size, ui.Color fgColor = Colors.black, ui.Color bgColor = Colors.white}) async {
     QrPainter qr = QrPainter(
@@ -88,31 +22,6 @@ class Rasterizer {
 
     ByteData qrBytes = await qr.toImageData(size);
     return Uint8List.sublistView(qrBytes);
-  }
-
-  Future<Uint8List> toImg(
-      {String text,
-      double width,
-      double height,
-      double fontSize = 20,
-      String fontFamily = "Roboto",
-      ui.Color fgColor = Colors.black,
-      ui.Color bgColor = Colors.transparent}) async {
-    TextImage txtImage = TextImage();
-    txtImage.text = text;
-    txtImage.fontSize = fontSize;
-    txtImage.fontFamily = fontFamily;
-    txtImage.fontColor = fgColor;
-    txtImage.bgColor = bgColor;
-    ui.ImageByteFormat format = ui.ImageByteFormat.png;
-    txtImage.text = text;
-    final recorder = ui.PictureRecorder();
-    final canvas = Canvas(recorder);
-    txtImage.paint(canvas, Size(width, height));
-    ui.Picture picture = recorder.endRecording();
-    ui.Image image = await picture.toImage(width.toInt(), height.toInt());
-    ByteData data = await image.toByteData(format: format);
-    return Uint8List.sublistView(data);
   }
 
   Future<ui.Image> rasterize({Wallet wallet, Art art}) async {
@@ -154,7 +63,7 @@ class Rasterizer {
     assert(artE != null);
     ui.Paint p = ui.Paint();
     p.color = artE.bgcolor;
-    canvas.drawRect(ui.Rect.fromLTRB(artE.left, artE.top, artE.width, artE.height), p);
+    // canvas.drawRect(ui.Rect.fromLTRB(artE.left, artE.top, artE.width, artE.height), p);
     TextSpan span = new TextSpan(style: new TextStyle(color: artE.fgcolor, fontSize: artE.size, fontFamily: "Roboto"), text: text);
     TextPainter tp = new TextPainter(text: span, textAlign: TextAlign.left, textDirection: TextDirection.ltr);
     double rad = (artE.rotation / 180) * math.pi;
