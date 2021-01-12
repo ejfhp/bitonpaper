990de99778176823ffa6d5752c2e041a28f5e919
ejfhp <ejfhp5@gmail.com>
2021-01-12 23:15:26 +0100

    Move html outside Status

--- flutter/lib/html_print.dart
+++ flutter/lib/html_print.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'BOPState.dart';
 import 'paper.dart';
 import 'dart:convert' show base64;
 import 'dart:html' as html;
@@ -11,10 +10,10 @@ const SAFE_WIDTH = 670;
 const SAFE_HEIGHT = 944;
 const MARGIN = 10;
 
-class PrintSheet extends StatelessWidget {
+class PrintSheetHTML extends StatelessWidget {
   final List<Paper> papers;
 
-  PrintSheet(this.papers);
+  PrintSheetHTML(this.papers);
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +21,7 @@ class PrintSheet extends StatelessWidget {
   }
 
   Future<void> preparePrintPreview(int ppp) async {
+    print("HTML_PRINT preparePrintPreview");
     int numPages = (papers.length / ppp).ceil();
     int creditsHeight = 8;
     int wi = 0;
@@ -32,8 +32,7 @@ class PrintSheet extends StatelessWidget {
     double scale = math.min(scaleW, scaleH);
     int width = (papers.first.width * scale).floor();
     int height = (papers.first.height * scale).floor();
-    print("HTML_PRINT printing page: v02");
-    temporarySetBodyStyleNotPositionFixed(true);
+    _temporarySetBodyStyleNotPositionFixed(true);
     html.DivElement printPreview = html.DivElement();
     printPreview.style.setProperty("position", "relative");
     html.querySelector("#printpreview").children.add(printPreview);
@@ -51,14 +50,17 @@ class PrintSheet extends StatelessWidget {
       printedWith.innerText = "Printed with bop.run @boprun";
       for (int pp = 0; pp < ppp && wi < wl; pp++) {
         print("Adding wi:" + wi.toString());
-        html.DivElement pDiv = await makePaperDiv(paper: papers[wi++], width: width, height: height);
+        html.DivElement pDiv = await _makePaperDiv(paper: papers[wi++], width: width, height: height);
         page.insertAdjacentElement("beforeEnd", pDiv);
       }
       page.insertAdjacentElement("beforeEnd", printedWith);
     }
+    //Wait a little to allow chrome on android to complete the draw
+    print("HTMLPRINT waiting 500 millis... ");
+    await Future.delayed(const Duration(milliseconds: 500), () {});
   }
 
-  Future<html.DivElement> makePaperDiv({Paper paper, int height, int width}) async {
+  Future<html.DivElement> _makePaperDiv({Paper paper, int height, int width}) async {
     Completer bgdComp = Completer();
     Completer ovrComp = Completer();
     html.DivElement paperDiv = html.DivElement();
@@ -96,11 +98,11 @@ class PrintSheet extends StatelessWidget {
 
   showPrintPreview() {
     html.window.print();
-    temporarySetBodyStyleNotPositionFixed(false);
+    _temporarySetBodyStyleNotPositionFixed(false);
     html.querySelector("#printpreview").children.clear();
   }
 
-  void temporarySetBodyStyleNotPositionFixed(bool removePosition) {
+  void _temporarySetBodyStyleNotPositionFixed(bool removePosition) {
     html.HtmlDocument doc = html.window.document as html.HtmlDocument;
     if (removePosition) {
       print("HTMLPRINT remove BODY style psition fixed!");
