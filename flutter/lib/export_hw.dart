1500f4a23f3fa58db8cd759fe241fc1d6257625b
ejfhp <ejfhp5@gmail.com>
2021-01-25 19:52:19 +0100

    Adding Android WIP

---
+++ flutter/lib/export_hw.dart
@@ -0,0 +1,5 @@
+import 'dart:typed_data';
+
+void openDownloadHTML(Uint8List data, String mime, String fileName) {
+  throw UnsupportedError('hw_none message');
+}
---
+++ flutter/lib/printer.dart
@@ -0,0 +1,6 @@
+import 'paper.dart';
+import 'package:flutter/material.dart';
+
+printPages(BuildContext context, List<Paper> papers, int walletsPP) async {
+  throw UnsupportedError('hw_none message');
+}
