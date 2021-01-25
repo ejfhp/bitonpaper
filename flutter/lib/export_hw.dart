722e0266042dc7a983192609e13b3156fb741665
ejfhp <ejfhp5@gmail.com>
2021-01-25 20:01:52 +0100

    Adding Android WIP

--- flutter/lib/export_hw.dart
+++ flutter/lib/export_hw.dart
@@ -1,5 +1,5 @@
 import 'dart:typed_data';
 
-void openDownloadHTML(Uint8List data, String mime, String fileName) {
+void openDownload(Uint8List data, String mime, String fileName) {
   throw UnsupportedError('hw_none message');
 }
